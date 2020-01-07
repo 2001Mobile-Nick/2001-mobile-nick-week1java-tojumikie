06ae0d38f2b2324a39e5ebc2c945d9fda1c1cbff
toju mikie <tojumikie@live.com>
2020-01-07 13:49:38 -0500

    corrected problem 4

--- src/main/java/com/revature/eval/java/core/EvaluationService.java
+++ src/main/java/com/revature/eval/java/core/EvaluationService.java
@@ -116,8 +116,22 @@ public class EvaluationService {
 	 * @return
 	 */
 	public int getScrabbleScore(String string) {
-		// TODO Write an implementation for this method declaration
-		return 0;
+        int ScrabblePoints = 0;
+        char[] letters = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 
+        'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+        int[] points = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
+        for(int i = 0; i < string.length(); i++)
+        {
+            for(int j = 0; j < 26; j++)
+            {
+                if(string.charAt(i) == letters[j])
+                {
+                    ScrabblePoints += points[j];
+                }
+            }
+        }
+        //System.out.println(ScrabblePoints);
+		return ScrabblePoints;
 	}
 
 	/**
