59d37d2c0b83f9f0e4ca2017854425767b1c7e9a
toju mikie <tojumikie@live.com>
2020-01-08 16:31:04 -0500

    completed problem 5

--- src/main/java/com/revature/eval/java/core/EvaluationService.java
+++ src/main/java/com/revature/eval/java/core/EvaluationService.java
@@ -174,8 +174,9 @@ public class EvaluationService {
 	 * NANP-countries, only 1 is considered a valid country code.
 	 */
 	public String cleanPhoneNumber(String string) {
-		// TODO Write an implementation for this method declaration
-		return null;
+        string = string.replaceAll("[^0-9]", "");
+        //System.out.println(string);
+        return string;
 	}
 
 	/**
